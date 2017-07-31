2274712fb2d38753936dd1326cb12db49bbaadcd
jabosnola <jabosnola@gmail.com>
2017-08-01 01:23:01 +0900

    Fixed inet_pton

--- main.c
+++ main.c
@@ -29,8 +29,8 @@ int main(int argc, char *argv[])
 	}
 
 	dev = argv[1];
-	inet_aton(argv[2], &sender.ip);
-	inet_aton(argv[3], &target.ip);
+	inet_pton(AF_INET, argv[2], &sender.ip);
+	inet_pton(AF_INET, argv[3], &target.ip);
 
 	handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
 	if (handle == NULL) {
